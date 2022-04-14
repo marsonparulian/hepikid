a7f7544b8a3c1f45185888ecc09776b60accd34d
marson parulian <marson.parulian@gmail.com>
2022-04-14 15:03:44 +1000

    init helper to web login

---
+++ src/web-login.ts
@@ -0,0 +1,9 @@
+import { Page } from "puppeteer"
+
+// This file contains helpers to automate puppeteer's page login to Shoppee seller center
+
+const login = async (page: Page): Promise<any> => {
+    // TODO Login if needed
+}
+
+export default login;
